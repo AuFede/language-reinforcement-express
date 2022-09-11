6ea49dc5b35ea8c9b166737fc8bde9b1d6f28743
AuFede <afopostingbox@gmail.com>
2022-09-11 20:12:40 -0300

    Formas básicas de cómo levantar un Server con express

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ https://www.youtube.com/watch?v=JmJ1WUoUIK4
 1. Express es un framework de NodeJS.
 2. Express es un framework unopinionated: Esto quiere decir que no nos da indicaciones
 de cómo escribir el código.
-3. Es importante destacar que Express al igual que NodeJS, usa el modelo cliente-servidor: HTTP (REQUEST-RESPONSE).
+3. Es importante destacar que express al igual que NodeJS, usa el modelo cliente-servidor: HTTP (REQUEST-RESPONSE).
+4. root: __dirname sirve para que el archivo sea leido desde su directorio inicial. __dirname sirve para ejecutar la ruta completa desde el sistema inicial hasta el proyecto.
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const http = require('http')
-const fs = require('fs')
+// http.createServer() crea un Servidor básico.
+// fs.createReadStream() permite leer el archivo de a poco.
+// pipe sirve para conectar uno o más streams.
 
-const server = http.createServer((req, res) => {
-   const read = fs.createReadStream('./static/index.html')
-   read.pipe(res)
-})
+// ----- PRIMER MÉTODO -----
 
-server.listen(3000)
-console.log(`Server on port ${3000}`)
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//    const read = fs.createReadStream('./static/index.html')
+//    read.pipe(res)
+// })
+
+// server.listen(3000)
+// console.log(`Server on port ${3000}`)
+
+//   ----- SEGUNDO MÉTODO: MÁS UTILIZADO. -----
+
+const express = require ('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+   res.sendFile ('./static/index.html', {
+      root: __dirname
+   })
+}) 
+
+app.listen(3000)
+console.log(`Escuchando en el puerto ${3000}`)
